5609cd3d0c4d4828d57dcf73e4a94a79015ba29a
ob-courtney <59902618+ob-courtney@users.noreply.github.com>
2020-04-28 21:18:45 -0700

    updating credentials form

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
 
 const express = require('express');
 const path = require('path');
+const { body,validationResult } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
 
 /**
  * App Variables
@@ -14,21 +16,39 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || '8000';
 
+var shop
+var apikey
+var password
+
 /**
  *  App Configuration
  */
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+app.use(express.json());
 
 /**
  * Routes Definitions
  */
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.render('index', {title: 'Home'});
   });
+
+app.get('/user', (req, res) => {
+    res.render('user', { title: 'Profile', userProfile: { nickname: 'Orderbot' } });
+});
+
+app.get('/save-credentials', function(req, res){
+    shop = req.query.user.shop;
+    apikey = req.query.user.apikey;
+    password = req.query.user.password;
+
+    res.send('Shop entered');
+});
 
 /**
  * Server Activation
